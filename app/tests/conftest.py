69caafd686859e8f3bad05ff6701aa3a1b77008a
Harsh Jyoriya <harshjyoriyagdg24@gmail.com>
2026-03-23 14:48:33 +0530

    Add test file

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,16 +1,24 @@
 from app.database.main import get_session
+from app.auth.dependencies import AccessTokenBearer, RoleChecker, RefreshTokenBearer
 from app import app
 from fastapi.testclient import TestClient
-from unittest.mock import Mock
+from unittest.mock import AsyncMock
 import pytest
 
-mock_session = Mock()
-mock_user_service = Mock()
+mock_session = AsyncMock()
+mock_user_service = AsyncMock()
+mock_book_service = AsyncMock()
 
 def get_mock_session():
     yield mock_session
 
+access_token_bearer = AccessTokenBearer()
+refresh_token_bearer = RefreshTokenBearer()
+role_checker = RoleChecker(['admin'])
+
 app.dependency_overrides[get_session] = get_mock_session
+app.dependency_overrides[role_checker] = AsyncMock()
+app.dependency_overrides[refresh_token_bearer] = AsyncMock()
 
 
 @pytest.fixture
@@ -21,6 +29,11 @@ def fake_session():
 @pytest.fixture
 def fake_user_service():
     return mock_user_service
+
+
+@pytest.fixture
+def fake_book_service():
+    return mock_book_service
 
 
 @pytest.fixture
--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -1,9 +1,11 @@
+from app.auth.schemas import UserCreateModel
+
 auth_prefix = f"/api/v1/auth"
 
 def test_user_creation(fake_session, fake_user_service, test_client):
     signup_data ={
             "username": "Harsh Jyoriya",
-            "email": "harshjyoriya639khhk@gmail.com",
+            "email": "harshjyoriya639@gmail.com",
             "password": "facebookmetas"  
     }
     response = test_client.post(
@@ -11,4 +13,10 @@ def test_user_creation(fake_session, fake_user_service, test_client):
         json=signup_data
 
     )
+
+    user_data = UserCreateModel(**signup_data)
+
     assert fake_user_service.user_exists_called_once()
+    assert fake_user_service.user_exists__with(signup_data['email'], fake_session)
+    assert fake_user_service.user_exists_called_once()
+    assert fake_user_service.user_exists__with(user_data, fake_session)
